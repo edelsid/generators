42b141a78547ca1e6cdb07b188ba4645567f0c86
edelsid <eradonna@yandex.ru>
2023-08-25 15:56:53 +0300

    method added

--- src/generator.js
+++ src/generator.js
@@ -3,7 +3,7 @@ export default class Team {
     this.size = size;
   }
 
-  * [Symbol.iterator]() {
+  static fill() {
     const names = ['Lisette', 'Morren', 'Fafnir', 'Constance', 'Eustacia'];
     const types = ['Bowman', 'Swordsman', 'Magician'];
     const health = 100;
@@ -11,14 +11,19 @@ export default class Team {
     const attacks = [25, 10, 25, 40, 30];
     const defences = [25, 10, 25, 40, 30];
 
+    const name = names[Math.floor(Math.random() * names.length)];
+    const type = types[Math.floor(Math.random() * types.length)];
+    const attack = attacks[Math.floor(Math.random() * attacks.length)];
+    const defence = defences[Math.floor(Math.random() * defences.length)];
+    const value = {
+      name, type, health, level, attack, defence,
+    };
+    return value;
+  }
+
+  * [Symbol.iterator]() {
     for (let i = 0; i < this.size; i += 1) {
-      const name = names[Math.floor(Math.random() * names.length)];
-      const type = types[Math.floor(Math.random() * types.length)];
-      const attack = attacks[Math.floor(Math.random() * attacks.length)];
-      const defence = defences[Math.floor(Math.random() * defences.length)];
-      yield {
-        name, type, health, level, attack, defence,
-      };
+      yield Team.fill();
     }
     return { value: undefined };
   }
